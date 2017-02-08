3fd2cbcf6346ca30375a2ef2ff0569a037ef539c
Mads Kempf <madskempf@hotmail.com>
2017-02-08 13:40:36 +0100

    Update Exercise1.java

--- exercises/src/cp/week6/Exercise1.java
+++ exercises/src/cp/week6/Exercise1.java
@@ -11,4 +11,26 @@ public class Exercise1
 	- Make Counter Thread-safe.
 	- Does it make a different to declare i private or public?
 	*/
+	
+	private static class counter{
+		
+		private int i;
+		
+		public counter(int i){
+			this.i = i;
+		}
+		
+		private void increment(){
+			this.i = this.i + 1;
+		}
+		
+		private void decrement(){
+			this.i = this.i - 1;
+		}
+		
+		private int getVal(){
+			return i;
+		}
+	}
+
 }
